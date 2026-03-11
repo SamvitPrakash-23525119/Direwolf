94a619fab4864c861515a89fa6fae4a17f89f6e3
Samvit Prakash <samvitprakash2004@gmail.com>
2026-03-11 18:45:43 +0200

    Add Clock widget and enhance app initialization with instance name and logging

--- ags/app.tsx
+++ ags/app.tsx
@@ -1,11 +1,24 @@
 import app from "ags/gtk4/app"
 import { Astal } from "ags/gtk4"
+import Gdk from "gi://Gdk?version=4.0"
 
 import css from './styles/dist/main.css'
+import Clock from "./modules/widgets/Clock"
 
 app.start({
     css: css,
+    instanceName: "DireWolf",
     main() {
+        console.log("Started AGS...");
         
+        return(
+            <window
+                class={"testing-window"}
+                anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.RIGHT}
+                visible
+            >
+                <Clock className={"clock"} format={'%H:%M'} />
+            </window>
+        );
     },
 })
