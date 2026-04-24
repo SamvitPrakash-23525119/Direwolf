439fdbcfc97bcaece44b3fc30d8f71b439fe1e4c
Samvit Prakash <samvitprakash2004@gmail.com>
2026-04-24 17:23:58 +0200

    Update TrayBar to show dynamic Wi-Fi icon
    Bind to AstalNetwork wifi enabled/icon via gnim to update iconName.
    Set pixelSize to 20. Fix SCSS indentation and refresh generated CSS map.

--- ags/modules/widgets/TrayBar.tsx
+++ ags/modules/widgets/TrayBar.tsx
@@ -1,7 +1,22 @@
+import Network from "gi://AstalNetwork"
+import { createEffect, createState, createBinding } from "gnim"
+
 export default function TrayBar() {
+	const [wifi_icon, set_wifi_icon] = createState("")
+
+	const nm = Network.get_default()
+
+	createEffect(() => {
+		const enabled = createBinding(nm.wifi, "enabled")
+		const icon = createBinding(nm.wifi, "icon_name")
+
+		if (enabled()) set_wifi_icon(icon())
+		else set_wifi_icon("network-wireless-offline-symbolic")
+	})
+
 	return (
 		<box $type="end" class={"tray-bar"}>
-			<image class={"icon"} iconName={"network-wireless-10-symbolic"} />
+			<image class={"icon"} iconName={wifi_icon((t) => t)} pixelSize={20} />
 			<image class={"icon"} iconName={"bluetooth-symbolic"} />
 			<image class={"icon"} iconName={"audio-volume-high-symbolic"} />
 			<image class={"icon"} iconName={"application-menu-symbolic"} />
