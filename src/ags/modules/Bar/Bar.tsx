866c6ff584ee651683f17e8c15f4cd02a634d2f5
Samvit Prakash <samvitprakash2004@gmail.com>
2026-05-07 00:34:18 +0200

    Refactor packaging and release workflow
    Rename binaries to lowercase 'direwolf' and adjust CI steps
    to build tar/zip (add checksum). Simplify makefile (NAME
    variable, DIST_FILE) and add package/cleanup targets. Remove
    legacy Arch packaging scripts and dependencies.json. Remove a
    debug console.log and reformat Bar component.

--- src/ags/modules/Bar/Bar.tsx
+++ src/ags/modules/Bar/Bar.tsx
@@ -4,26 +4,26 @@ import TrayBar from "../widgets/TrayBar"
 import { Astal } from "ags/gtk4"
 
 type BarProps = {
-  setMediaCard: (mediaCardState: boolean) => void
+	setMediaCard: (mediaCardState: boolean) => void
 }
 
 export default function Bar({ setMediaCard }: BarProps) {
-  return (
-    <window
-      class={"top-bar-window"}
-      anchor={
-        Astal.WindowAnchor.TOP |
-        Astal.WindowAnchor.RIGHT |
-        Astal.WindowAnchor.LEFT
-      }
-      exclusivity={Astal.Exclusivity.EXCLUSIVE}
-      visible
-    >
-      <centerbox hexpand>
-        <MediaBar setMediaCard={setMediaCard} />
-        <WorkspaceBar />
-        <TrayBar />
-      </centerbox>
-    </window>
-  )
+	return (
+		<window
+			class={"top-bar-window"}
+			anchor={
+				Astal.WindowAnchor.TOP |
+				Astal.WindowAnchor.RIGHT |
+				Astal.WindowAnchor.LEFT
+			}
+			exclusivity={Astal.Exclusivity.EXCLUSIVE}
+			visible
+		>
+			<centerbox hexpand>
+				<MediaBar setMediaCard={setMediaCard} />
+				<WorkspaceBar />
+				<TrayBar />
+			</centerbox>
+		</window>
+	)
 }
