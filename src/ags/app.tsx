880d6f488311671e1ce19525290b46cad1c4a1b0
SamvitPrakash-23525119 <samvitprakash2004@gmail.com>
2026-07-04 20:39:21 +0200

    Removed obsolete code files for legacy rewrite

--- src/ags/app.tsx
+++ src/ags/app.tsx
@@ -1,25 +1,11 @@
 import app from "ags/gtk4/app"
 import css from "./styles/dist/main.css"
-import Bar from "./modules/Bar/Bar"
-import MediaCard from "./modules/MediaCard/MediaCard"
-import VolumeModal from "./modules/VolumeModal/VolumeModal"
-import { createState } from "gnim"
 
 app.start({
   css: css,
   instanceName: "Direwolf",
   iconTheme: "Adwaita",
   main() {
-    const [mediaCard, setMediaCard] = createState(false)
-
     console.log("Started AGS...")
-
-    return (
-      <>
-        <Bar setMediaCard={setMediaCard} />
-        <MediaCard visible={mediaCard} />
-        <VolumeModal />
-      </>
-    )
   },
 })
