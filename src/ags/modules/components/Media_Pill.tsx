34a995811c313648ef6f212aa9575f2f7889d7e1
SamvitPrakash-23525119 <samvitprakash2004@gmail.com>
2026-07-05 08:34:16 +0200

    Add MediaPill component and integrate into Bar; update styles for media controls

---
+++ src/ags/modules/components/Media_Pill.tsx
@@ -0,0 +1,35 @@
+import { ICON_SIZE } from "../../constants/icons"
+
+export default function MediaPill(){
+    return (
+        <box
+            $type='start'
+            spacing={10}
+            class={"top-bar media-pill"}
+        >
+            <image 
+                file={'/home/_c3rberus/GitHub/Direwolf/assets/screenshots/desktop-before.png'} 
+                class={'media-album-cover'}
+                pixelSize={30}
+            />
+            
+            <label label={'Stay and Decay'} class={'media-label small'}/>
+
+            <box>
+                <button class={'media-control-button'} >
+                    <image iconName={'media-skip-backward-symbolic'} class={'icon media-icon'} pixelSize={ICON_SIZE}/>
+                </button>
+
+                <button class={'media-control-button'} >
+                    <image iconName={'media-playback-start-symbolic'} class={'icon media-icon'} pixelSize={ICON_SIZE}/>
+                </button>
+
+                <button class={'media-control-button'} >
+                    <image iconName={'media-skip-forward-symbolic'} class={'icon media-icon'} pixelSize={ICON_SIZE}/>
+                </button>
+            </box>
+
+        </box>
+    )
+
+}
