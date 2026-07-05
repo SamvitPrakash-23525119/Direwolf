97b7b0906668083f7c499fbd4880d772782a62b2
SamvitPrakash-23525119 <samvitprakash2004@gmail.com>
2026-07-05 20:12:21 +0200

    Refactor component styles and spacing; update Start Button, System Tray, Time components, and add new styles for system tray

--- src/ags/modules/components/Start_Button.tsx
+++ src/ags/modules/components/Start_Button.tsx
@@ -4,7 +4,8 @@ export default function StartButton(){
     return (
         <box class={"top-bar messages-bar start-bar"}>
             <button  class={'messages-button start-button'}>
-                <image file={'/home/_c3rberus/GitHub/Direwolf/assets/logos/direwolf4.svg'} class={"icon start-icon"} pixelSize={ICON_SIZE+12}/>
+                {/* <image file={'/home/_c3rberus/GitHub/Direwolf/assets/logos/direwolf4.svg'} class={"icon start-icon"} pixelSize={ICON_SIZE+12}/> */}
+                <image iconName={'view-app-grid-symbolic'} class={"icon start-icon"} pixelSize={ICON_SIZE+12}/>
             </button>
         </box>
     )   
--- src/ags/modules/components/Time.tsx
+++ src/ags/modules/components/Time.tsx
@@ -2,7 +2,7 @@ import {ICON_SIZE} from '../../constants/icons';
 
 export default function Time(){
     return (
-        <box class={"top-bar"} spacing={8}>
+        <box class={"top-bar time-bar"} spacing={8}>
             <image iconName={'preferences-system-time-symbolic'} class={"icon"} pixelSize={ICON_SIZE}/>
             <label label={'17:32 | 2 Jul'} class={'time-label small nandinagari'}/>
         </box>
