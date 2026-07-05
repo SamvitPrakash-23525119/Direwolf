b3317fc68f63d8ca0ddd0977bd85a5b4dfbce0fd
SamvitPrakash-23525119 <samvitprakash2004@gmail.com>
2026-07-05 20:31:02 +0200

    Refactor Start Button and top-bar styles; adjust padding for improved layout

--- src/ags/modules/components/Start_Button.tsx
+++ src/ags/modules/components/Start_Button.tsx
@@ -5,7 +5,7 @@ export default function StartButton(){
         <box class={"top-bar messages-bar start-bar"}>
             <button  class={'messages-button start-button'}>
                 {/* <image file={'/home/_c3rberus/GitHub/Direwolf/assets/logos/direwolf4.svg'} class={"icon start-icon"} pixelSize={ICON_SIZE+12}/> */}
-                <image iconName={'view-app-grid-symbolic'} class={"icon start-icon"} pixelSize={ICON_SIZE+12}/>
+                <image iconName={'view-app-grid-symbolic'} class={"icon start-icon"} pixelSize={ICON_SIZE+8}/>
             </button>
         </box>
     )   
