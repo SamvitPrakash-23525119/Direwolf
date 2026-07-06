c698e67cca3e499b826ba49f2a70d9168cc55557
SamvitPrakash-23525119 <samvitprakash2004@gmail.com>
2026-07-06 21:28:56 +0200

    Integrate NmService into SystemTray component: enhance network icon and SSID bindings for improved network status display

--- src/ags/modules/components/System_Tray.tsx
+++ src/ags/modules/components/System_Tray.tsx
@@ -1,15 +1,56 @@
+import NmService from "../../services/shared_libraries/NmService";
 import { ICON_SIZE } from "../../constants/icons";
+import { createBinding } from "gnim";
 
 export default function SystemTray(){
+    const nm = NmService.get_default().getNm();
+
+    const primary = createBinding(nm, 'primary');
+    const wifi = createBinding(nm, 'wifi');
+
+    const wifiIcon = createBinding(nm.wifi, 'icon_name');
+    const wiredIcon = createBinding(nm.wired, 'icon_name');
+
+    const networkIcon = primary.as((primary) => {
+        switch(primary){
+            case 0:
+                return wifiIcon;
+
+            case 1:
+                return wiredIcon;
+
+            case 2:
+                return wifiIcon;
+
+        }
+    })
+
+    const wifiSSID = wifi.as((wifi) => wifi.ssid)
+
+    const networkSSID = primary.as((primary) => {
+        switch(primary){
+            case 0:
+                return wifiSSID;
+
+            case 1:
+                return 'Ethernet';
+
+            case 2:
+                return wifiSSID;
+
+        }
+    })
+
     return (
         <box 
             spacing={8} 
             class={"top-bar system-tray-bar"}
         >
             <image 
-                iconName={'network-wireless-signal-excellent-symbolic'} 
+                iconName={networkIcon()} 
                 class={"icon"} 
                 pixelSize={ICON_SIZE}
+                tooltipText={networkSSID()}
             />
             
             <image 
