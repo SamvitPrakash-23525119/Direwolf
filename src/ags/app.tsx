8bfb395111114e8f450cd3d246608c45efc7ad31
SamvitPrakash-23525119 <samvitprakash2004@gmail.com>
2026-07-08 01:22:08 +0200

    Implement media pill functionality with MprisService integration and UI updates

--- src/ags/app.tsx
+++ src/ags/app.tsx
@@ -1,6 +1,8 @@
 import app from "ags/gtk4/app"
 import css from "./styles/dist/main.css"
 import Bar from "./modules/widgets/Bar"
+import MprisService from "./services/shared_libraries/MprisService"
+import { createBinding, createEffect } from "ags"
 
 app.start({
 	css: css,
@@ -8,6 +10,26 @@ app.start({
 	iconTheme: "Adwaita",
 	main() {
 		console.log("Started AGS...")
+
+		const mpris = MprisService.get_default().getMpris();
+
+		// for(const i in mpris) console.log(i);
+
+		// console.log('+====================================================')
+
+		// console.log(mpris.get_items_type);
+
+		createEffect(() => {
+			const players = createBinding(mpris, "players");
+
+			for(const i in players()[0]) console.log(i);
+			
+			console.log('----------------------------------------')
+			
+			console.log(players()[0]?.identity);
+			
+			console.log('----------------------------------------')
+		})
 
 		return (
 			<>
