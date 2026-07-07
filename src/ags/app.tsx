5c68c759b5b128960a4e79393447ae2f078ce785
SamvitPrakash-23525119 <samvitprakash2004@gmail.com>
2026-07-07 14:12:45 +0200

    Remove unused WpctlService and related volume management code from app.tsx

--- src/ags/app.tsx
+++ src/ags/app.tsx
@@ -1,8 +1,6 @@
 import app from "ags/gtk4/app"
 import css from "./styles/dist/main.css"
 import Bar from "./modules/widgets/Bar"
-import WpctlService from "./services/shared_libraries/WpctlService"
-import { createBinding, createEffect } from "gnim"
 
 app.start({
 	css: css,
@@ -10,23 +8,6 @@ app.start({
 	iconTheme: "Adwaita",
 	main() {
 		console.log("Started AGS...")
-		const wpctl = WpctlService.get_default().getWpctl();
-
-		for (const i in wpctl.default_speaker) console.log(i);
-
-		console.log('--------------------------');
-
-
-		createEffect(() => {
-			const defaultSpeaker = createBinding(wpctl, 'default_speaker');
-			const volume = createBinding(defaultSpeaker(), 'volume');
-
-			console.log(volume());
-
-		})
-
-		console.log('muted:' ,wpctl.default_speaker.mute);
-
 
 		return (
 			<>
